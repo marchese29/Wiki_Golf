cd610058cbd69916549ecd071aba6fd369712902
marchese29 <marchese.daniel@gmail.com>
2013-12-09 22:00:31 -0500

    Added more scaffolding

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
 import flask
+from flask import request
+import json
+import re
+import requests
+
 app = flask.Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello():
-	return flask.render_template('hello.html', name='World!')
+	req = requests.get(''.join([
+		'http://en.wikipedia.org/w/api.php',
+		'?action=query',
+		'&prop=revisions',
+		'&rvprop=content',
+		'&format=json',
+		'&titles=computing'
+	]))
+
+	return re.findall('\[\[(?!Category:)(?!File:)(?!Image:).*?\]\]', req.text)
 
 if __name__ == '__main__':
 	app.debug = True
